fc73ace37c7c7fea185477f14c7e70fdbe942c98
Hansel <hansel.hir@gmail.com>
2024-06-28 17:20:29 -0500

    change to ResponseEntity by String endpoint  /signin

--- src/main/java/pe/creativity/Restfull/controller/UserController.java
+++ src/main/java/pe/creativity/Restfull/controller/UserController.java
@@ -5,12 +5,14 @@ import org.apache.juli.logging.LogFactory;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpServerErrorException;
 import pe.creativity.Restfull.dto.LoginDto;
 import pe.creativity.Restfull.entity.User;
 import pe.creativity.Restfull.service.UsuarioServiceImpl;
+import pe.creativity.Restfull.util.InvalidCredentialsException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -25,9 +27,15 @@ public class UserController {
     private UsuarioServiceImpl usuarioService;
 
     @PostMapping("/signin")
-    public String login(@RequestBody @Valid LoginDto loginDto) {
-        return usuarioService.signin(loginDto.getUsername(), loginDto.getPassword()).orElseThrow(() ->
-                new HttpServerErrorException(HttpStatus.FORBIDDEN, "Ingreso Fallido"));
+    public ResponseEntity<?> login(@RequestBody @Valid LoginDto loginDto) {
+        try {
+            String token = usuarioService.signin(loginDto.getUsername(), loginDto.getPassword()).orElseThrow(() ->
+                    //new HttpServerErrorException(HttpStatus.FORBIDDEN, "Ingreso Fallido"));
+                    new InvalidCredentialsException("Credenciales inválidas por username o password"));
+            return ResponseEntity.ok(token);
+        } catch (InvalidCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        }
     }
 
     @PostMapping("/signout")
