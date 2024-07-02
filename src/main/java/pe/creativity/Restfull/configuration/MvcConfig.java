06c527c4bafaedf9631dbfb1dee11f4882e7ae1f
Hansel <hansel.hir@gmail.com>
2024-07-01 22:38:45 -0500

    add config cors allow

--- src/main/java/pe/creativity/Restfull/configuration/MvcConfig.java
+++ src/main/java/pe/creativity/Restfull/configuration/MvcConfig.java
@@ -1,16 +1,15 @@
-//package pe.creativity.Restfull.configuration;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class MvcConfig implements WebMvcConfigurer {
-//
-//    public void addViewControllers(ViewControllerRegistry registry){
-//        registry.addViewController("/").setViewName("Home");
-//        registry.addViewController("/login").setViewName("login");
-//        registry.addViewController("/swagger-ui").setViewName("SWAGGER");
-//    }
-//
-//}
+package pe.creativity.Restfull.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry){
+        registry.addViewController("/").setViewName("Home");
+        registry.addViewController("/login").setViewName("sigin.html");
+        registry.addViewController("/swagger-ui").setViewName("SWAGGER");
+    }
+}
--- src/main/java/pe/creativity/Restfull/configuration/WebSecurityConfig.java
+++ src/main/java/pe/creativity/Restfull/configuration/WebSecurityConfig.java
@@ -1,18 +1,24 @@
 package pe.creativity.Restfull.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import pe.creativity.Restfull.Security.CustomUserDetailsService;
-import pe.creativity.Restfull.Security.JwtAutheticationTokenFilter;
-import pe.creativity.Restfull.Security.JwtAuthorizationTokenFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import pe.creativity.Restfull.security.CustomUserDetailsService;
+import pe.creativity.Restfull.security.JwtAutheticationTokenFilter;
+import pe.creativity.Restfull.security.JwtAuthorizationTokenFilter;
+import pe.creativity.Restfull.security.JwtProvider;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,33 +28,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private CustomUserDetailsService customUserDetailsService;
 
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/Home").permitAll()
+                .antMatchers("/", "/Home", "/swagger-ui/index.html", "/v2/api-docs", "/swagger-resources/configuration/ui").permitAll()
                 .antMatchers("/login").permitAll()
                 .antMatchers("/users/signin").permitAll()
                 .antMatchers("/users/signout").permitAll()
                 .antMatchers("/v1").authenticated()
                 //no permitir a todos los demas
                 .anyRequest().authenticated()
-              //  .and()    retirar
-             //   .formLogin()    retirar
+                //  .and()    retirar
+                //   .formLogin()    retirar
                 // .loginPage("/log-in")
-          //      .permitAll()    retirar
+                //      .permitAll()    retirar
                 .and()
                 .logout()
                 .permitAll()
                 .and()
                 .addFilter(new JwtAutheticationTokenFilter(authenticationManager()))
-                .addFilter(new JwtAuthorizationTokenFilter(authenticationManager()));;
-
+                .addFilter(new JwtAuthorizationTokenFilter(authenticationManager(), jwtProvider, secretKey));
         // Disable CSRF (cross site request forgery)
         http.csrf().disable();
 
     }
-
 
 //    @Bean
 //    public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -64,5 +74,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
