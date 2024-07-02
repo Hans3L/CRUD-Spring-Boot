7e4817971dedab9801d04398c4e1c3d5d52b7218
Hansel <hansel.hir@gmail.com>
2024-07-01 22:31:59 -0500

    delete space

--- src/main/java/pe/creativity/Restfull/util/Constants.java
+++ src/main/java/pe/creativity/Restfull/util/Constants.java
@@ -1,7 +1,7 @@
 package pe.creativity.Restfull.util;
 
 public class Constants {
-    
+
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
 
